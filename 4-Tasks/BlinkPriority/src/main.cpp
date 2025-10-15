118eb817897a2bb2bb8628724095adefd0bbf00d
thomasdegoul <thom2gl.etc@gmail.com>
2025-10-15 10:41:04 +0800

    Update main.cpp

--- 4-Tasks/BlinkPriority/src/main.cpp
+++ 4-Tasks/BlinkPriority/src/main.cpp
@@ -4,92 +4,78 @@
  * Jon Durrant
  * 15-Aug-2022
  */
-
-
 #include "pico/stdlib.h"
-
 #include "FreeRTOS.h"
 #include "task.h"
 #include <stdio.h>
-
 #include "BlinkAgent.h"
 #include "BlinkWorker.h"
 #include "BlinkHeavy.h"
 
-
-//Standard Task priority
+// Standard Task priority
 #define TASK_PRIORITY		( tskIDLE_PRIORITY + 1UL )
-
-//LED PAD to use
+// LED PAD to use
 #define LED_PAD				0
 #define LED1_PAD			2
 #define LED2_PAD			3
 
-
-
-void runTimeStats(   ){
+void runTimeStats() {
 	TaskStatus_t *pxTaskStatusArray;
 	volatile UBaseType_t uxArraySize, x;
 	unsigned long ulTotalRunTime;
 
+	// Get number of tasks
+	uxArraySize = uxTaskGetNumberOfTasks();
+	printf("Number of tasks %d\n", uxArraySize);
 
-   // Get number of takss
-   uxArraySize = uxTaskGetNumberOfTasks();
-   printf("Number of tasks %d\n", uxArraySize);
+	// Allocate a TaskStatus_t structure for each task
+	pxTaskStatusArray = (TaskStatus_t *)pvPortMalloc( uxArraySize * sizeof( TaskStatus_t ) );
+	if( pxTaskStatusArray != NULL ) {
+		// Generate raw status information about each task
+		uxArraySize = uxTaskGetSystemState( pxTaskStatusArray, uxArraySize, &ulTotalRunTime );
 
-   //Allocate a TaskStatus_t structure for each task.
-   pxTaskStatusArray = (TaskStatus_t *)pvPortMalloc( uxArraySize * sizeof( TaskStatus_t ) );
-
-   if( pxTaskStatusArray != NULL ){
-      // Generate raw status information about each task.
-      uxArraySize = uxTaskGetSystemState( pxTaskStatusArray,
-                                 uxArraySize,
-                                 &ulTotalRunTime );
-
-	 // Print stats
-	 for( x = 0; x < uxArraySize; x++ )
-	 {
-		 printf("Task: %d \t cPri:%d \t bPri:%d \t hw:%d \t%s\n",
-				pxTaskStatusArray[ x ].xTaskNumber ,
-				pxTaskStatusArray[ x ].uxCurrentPriority ,
-				pxTaskStatusArray[ x ].uxBasePriority ,
-				pxTaskStatusArray[ x ].usStackHighWaterMark ,
+		// Print stats
+		for( x = 0; x < uxArraySize; x++ ) {
+			printf("Task: %d \t cPri:%d \t bPri:%d \t hw:%d \t%s\n",
+				pxTaskStatusArray[ x ].xTaskNumber,
+				pxTaskStatusArray[ x ].uxCurrentPriority,
+				pxTaskStatusArray[ x ].uxBasePriority,
+				pxTaskStatusArray[ x ].usStackHighWaterMark,
 				pxTaskStatusArray[ x ].pcTaskName
-				);
-	 }
+			);
+		}
+		// Free array
+		vPortFree( pxTaskStatusArray );
+	} else {
+		printf("Failed to allocate space for stats\n");
+	}
 
-
-      // Free array
-      vPortFree( pxTaskStatusArray );
-   } else {
-	   printf("Failed to allocate space for stats\n");
-   }
-
-   //Get heap allocation information
-   HeapStats_t heapStats;
-   vPortGetHeapStats(&heapStats);
-   printf("HEAP avl: %d, blocks %d, alloc: %d, free: %d\n",
-		   heapStats.xAvailableHeapSpaceInBytes,
-		   heapStats.xNumberOfFreeBlocks,
-		   heapStats.xNumberOfSuccessfulAllocations,
-		   heapStats.xNumberOfSuccessfulFrees
-		   );
+	// Get heap allocation information
+	HeapStats_t heapStats;
+	vPortGetHeapStats(&heapStats);
+	printf("HEAP avl: %d, blocks %d, alloc: %d, free: %d\n",
+		heapStats.xAvailableHeapSpaceInBytes,
+		heapStats.xNumberOfFreeBlocks,
+		heapStats.xNumberOfSuccessfulAllocations,
+		heapStats.xNumberOfSuccessfulFrees
+	);
 }
-
 
 /***
  * Main task to blink external LED
  * @param params - unused
  */
-void mainTask(void *params){
+void mainTask(void *params) {
 	BlinkAgent blink(LED_PAD);
 	BlinkWorker worker1(LED1_PAD);
 	BlinkHeavy worker2(LED2_PAD);
-
 	printf("Main task started\n");
 
-	blink.start("Blink", TASK_PRIORITY);
-	worker1.start("Worker 1", TASK_PRIORITY+ 1);
+	// BlinkAgent now has the highest priority
+	blink.start("Blink", TASK_PRIORITY + 3);
+
+	// Other tasks have lower priorities
+	worker1.start("Worker 1", TASK_PRIORITY + 1);
 	worker2.start("Worker 2", TASK_PRIORITY + 2);
 
 	while (true) { // Loop forever
@@ -98,38 +84,32 @@ void mainTask(void *params){
 	}
 }
 
-
-
-
 /***
  * Launch the tasks and scheduler
  */
-void vLaunch( void) {
+void vLaunch( void ) {
+	// Start blink task
+	TaskHandle_t task;
+	xTaskCreate(mainTask, "MainThread", 500, NULL, TASK_PRIORITY, &task);
 
-	//Start blink task
-    TaskHandle_t task;
-    xTaskCreate(mainTask, "MainThread", 500, NULL, TASK_PRIORITY, &task);
-
-    /* Start the tasks and timer running. */
-    vTaskStartScheduler();
+	// Start the tasks and timer running
+	vTaskStartScheduler();
 }
 
 /***
  * Main
  * @return
  */
-int main( void )
-{
-	//Setup serial over USB and give a few seconds to settle before we start
-    stdio_init_all();
-    sleep_ms(2000);
-    printf("GO\n");
+int main( void ) {
+	// Setup serial over USB and give a few seconds to settle before we start
+	stdio_init_all();
+	sleep_ms(2000);
+	printf("GO\n");
 
-    //Start tasks and scheduler
-    const char *rtos_name = "FreeRTOS";
-    printf("Starting %s on core 0:\n", rtos_name);
-    vLaunch();
-
-
-    return 0;
+	// Start tasks and scheduler
+	const char *rtos_name = "FreeRTOS";
+	printf("Starting %s on core 0:\n", rtos_name);
+	vLaunch();
+	return 0;
 }
+
