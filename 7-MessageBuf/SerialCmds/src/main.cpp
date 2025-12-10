370b5e9d315391d15e74b8e38f0c2a963ba21b89
thomasdegoul <thom2gl.etc@gmail.com>
2025-12-10 10:33:36 +0800

    Update main.cpp

--- 7-MessageBuf/SerialCmds/src/main.cpp
+++ 7-MessageBuf/SerialCmds/src/main.cpp
@@ -24,12 +24,12 @@
 #define TASK_PRIORITY		( tskIDLE_PRIORITY + 1UL )
 
 //LED PAD to use
-#define LED_PAD				0
+#define LED_PAD				25
 #define LED1_PAD			2
 #define LED2_PAD			3
 #define LED3_PAD			4
 #define LED4_PAD			5
-#define LED5_PAD		   15
+#define LED5_PAD		    6
 
 
 void runTimeStats(   ){
